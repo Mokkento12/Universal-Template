39da9faf9e0cf33e2984579f5be8475ff0ebd08d
roman alekseev <roma.alekseev.90@bk.ru>
2024-12-06 16:49:35 +0300

    Добавлено сохранение темы в LocalStorage

--- src/context/ThemeContext.js
+++ src/context/ThemeContext.js
@@ -1,9 +1,15 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useEffect, useState } from "react";
 
 export const ThemeContext = createContext();
 
 export const ThemeProvider = ({ children }) => {
-  const [theme, setTheme] = useState("light");
+  const [theme, setTheme] = useState(() => {
+    return localStorage.getItem("theme") || "light";
+  });
+
+  useEffect(() => {
+    localStorage.setItem("theme", theme);
+  }, [theme]);
 
   const toggleTheme = () => {
     setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
